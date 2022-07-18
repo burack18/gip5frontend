9a5b57ed17bd2b2e33a02909b2ee4a16306ab1ae
burack <omerfaruk.aksak@student.ucll.be>
2022-07-18 19:31:29 +0200

    menu login register page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import Login from './components/Login';
+import Register from './components/Register';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './components/DashboardContent';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: "en",
+    detection: { order: ['cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'], caches: ['cookie'] },
+    backend:{loadPath: `/assets/locales/fr/translation.json`} 
+    });
+
 
 function App() {
+  const { t } = useTranslation();
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>    
+      <Route path='/login' element={<Login />} />
+      <Route path='register' element={<Register />} />
+      <Route path='dashport' element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
